68ed43480c73c872b738b1ed4dbc2588313bd7dd
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-09-28 15:40:47 +0200

    Création d'évenement tests

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EvenementRepository $evenementRepository): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $events = $evenementRepository->findAll();
+
+        $allEvents = [];
+
+        foreach($events as $event){
+            $allEvents[] = [
+                'id' => $event->getId(),
+                'start' => $event->getDateDebut()->format('Y-m-d H:i:s'),
+                'end' => $event->getDateFin()->format('Y-m-d H:i:s'),
+                'title' => $event->getNomEvenement(),
+            ];
+        }
+
+        $data = json_encode($allEvents);
+        return $this->render('home/index.html.twig', compact('data'));
     }
 }
